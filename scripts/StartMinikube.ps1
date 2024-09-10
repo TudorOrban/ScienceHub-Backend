f24a4fa84b2e474ff3b2bf8f553d9d6f6b728fa1
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-09-10 11:36:40 +0300

    Adjust minikube resource names

--- scripts/StartMinikube.ps1
+++ scripts/StartMinikube.ps1
@@ -1,3 +1,9 @@
+param (
+    [string]$buildCore = "false",
+    [string]$buildCommunity = "false",
+    [string]$buildApiGateway = "false"
+)
+
 Push-Location -Path ".."
 
 Write-Host "Deleting Minikube"
@@ -5,6 +11,17 @@ minikube delete
 
 Write-Host "Starting Minikube"
 minikube start --driver=docker
+
+Write-Host "Building Docker images"
+if ($buildCore -eq "true") {
+    docker build -t tudoraorban/sciencehub-backend-core:latest -f sciencehub-backend-core/Dockerfile .
+}
+if ($buildCommunity -eq "true") {
+    docker build -t tudoraorban/sciencehub-backend-community:latest -f sciencehub-backend-community/Dockerfile .
+}
+if ($buildApiGateway -eq "true") {
+    docker build -t tudoraorban/sciencehub-backend-api-gateway:latest -f sciencehub-backend-api-gateway/Dockerfile .
+}
 
 Write-Host "Applying configurations"
 Push-Location -Path "sciencehub-backend-core"
@@ -16,5 +33,8 @@ kubectl apply -f postgresql-core.yaml
 kubectl apply -f sciencehub-backend-core.yaml
 kubectl apply -f sciencehub-backend-community.yaml
 kubectl apply -f sciencehub-backend-api-gateway.yaml
+
+# kubectl port-forward service/postgresql-core-service 5432:5432
+# kubectl port-forward <api-gateway-pod-name> 8082:8082 
 
 Pop-Location
